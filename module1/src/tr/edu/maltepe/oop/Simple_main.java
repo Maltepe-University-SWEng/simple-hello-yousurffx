fed2cc170c61c0b5eb2fc2fea15472782890c831
yousurffx <153926736+yousurffx@users.noreply.github.com>
2024-10-08 10:52:59 +0300

    Update Simple_main.java
    Yusuf Talha Kamiloğlu - 220706006

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,10 +1,11 @@
 package tr.edu.maltepe.oop;
-public  class imple_main {
+public class Simple_main {
 
  public static void main(String []args){
         //
-       System.out.println("simple main.....");
+       System.out.println("YUSUF");
         }
 }
+
 
 
